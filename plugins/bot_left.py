4337519df2af91c5d61d4f56b30cd506b9f1b6d2
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-29 18:13:24 +0530

    Update bot_left.py

--- plugins/bot_left.py
+++ plugins/bot_left.py
@@ -8,11 +8,11 @@ from VIPMUSIC import app
 from VIPMUSIC.utils.database import delete_served_chat, get_assistant
 
 photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://envs.sh/qN8.jpg",
+    "https://envs.sh/qNk.jpg",
+    "https://envs.sh/qNl.jpg",
+    "https://envs.sh/qN4.jpg",
+    "https://envs.sh/qNU.jpg",
 ]
 
 
